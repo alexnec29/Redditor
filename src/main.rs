0a59afd03a8ac2c236cfbceaa2062b7667dd8921
alexnec29 <alexandrunec29@gmail.com>
2025-03-25 17:21:18 +0200

    Creating Loop of fetching posts

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use chrono::{DateTime, Local};
 use serde_json::Value;
 use std::env;
 use ureq::Error;
+use std::{thread,time};
 
 fn main() -> Result<(), ureq::Error> {
     let valid_sorts = [
@@ -16,6 +17,9 @@ fn main() -> Result<(), ureq::Error> {
     let subreddit = args.get(1).unwrap_or(&"rust".to_string()).clone();
     let mut sort = args.get(2).unwrap_or(&"hot".to_string()).clone();
 
+    let seconds=10;
+    let interval = time::Duration::from_millis(seconds * 1000);
+
     if !valid_sorts.contains(&sort) {
         eprintln!(
             "Invalid sort method: '{}'. Switching to default: 'hot'.",
@@ -25,49 +29,51 @@ fn main() -> Result<(), ureq::Error> {
     }
 
     let link = format!("https://www.reddit.com/r/{}/{}/.json", subreddit, sort);
+    loop{
+        let response = ureq::get(&link).call();
+        match response {
+            Ok(res) => {
+                let json: Value = serde_json::from_reader(res.into_reader()).unwrap();
+                //println!("Extracted JSON: {}", json);
 
-    let response = ureq::get(&link).call();
-    match response {
-        Ok(res) => {
-            let json: Value = serde_json::from_reader(res.into_reader()).unwrap();
-            //println!("Extracted JSON: {}", json);
+                let posts = match json["data"]["children"].as_array() {
+                    Some(posts) => posts,
+                    None => {
+                        eprintln!("Invalid json!");
+                        return Ok(());
+                    }
+                };
 
-            let posts = match json["data"]["children"].as_array() {
-                Some(posts) => posts,
-                None => {
-                    eprintln!("Invalid json!");
-                    return Ok(());
-                }
-            };
+                for post in posts {
+                    let post_data = &post["data"];
+                    let title = post_data["title"].as_str().unwrap_or("No title");
+                    let raw_permalink = post_data["permalink"].as_str().unwrap_or("");
+                    let permalink = format!("https://www.reddit.com{}", raw_permalink);
+                    let created_utc = post_data["created_utc"].as_f64().unwrap_or(0.0);
 
-            for post in posts {
-                let post_data = &post["data"];
-                let title = post_data["title"].as_str().unwrap_or("No title");
-                let raw_permalink = post_data["permalink"].as_str().unwrap_or("");
-                let permalink = format!("https://www.reddit.com{}", raw_permalink);
-                let created_utc = post_data["created_utc"].as_f64().unwrap_or(0.0);
-
-                let date_time =
-                    DateTime::from_timestamp(created_utc as i64, 0).expect("Invalid timestamp");
-                let local_date_time = date_time.with_timezone(&Local);
-                {
-                    println!("Title: {}", title);
-                    println!("Link to post: {}", permalink);
-                    println!("Creation date: {}", local_date_time);
-                    println!("");
+                    let date_time =
+                        DateTime::from_timestamp(created_utc as i64, 0).expect("Invalid timestamp");
+                    let local_date_time = date_time.with_timezone(&Local);
+                    {
+                        println!("Title: {}", title);
+                        println!("Link to post: {}", permalink);
+                        println!("Creation date: {}", local_date_time);
+                        println!("");
+                    }
                 }
             }
+            Err(Error::Status(code, res)) => {
+                let response_string = res.into_string()?;
+                eprintln!(
+                    "Response error of code {}. Reason: {}",
+                    code, response_string
+                );
+            }
+            Err(_) => {
+                eprintln!("Error at response!");
+            }
         }
-        Err(Error::Status(code, res)) => {
-            let response_string = res.into_string()?;
-            eprintln!(
-                "Response error of code {}. Reason: {}",
-                code, response_string
-            );
-        }
-        Err(_) => {
-            eprintln!("Error at response!");
-        }
+        println!("Printed the json! Waiting the interval...");
+        thread::sleep(interval);
     }
-    Ok(())
 }
